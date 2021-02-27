70e35c2fd217cde430d53a80bd8f188a444859c6
GitHub Action Runner <support+actions@github.com>
2021-02-27 05:02:17 +0000

    Updates

---
+++ _next/static/rNB21_U13zeHoB7yortau/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1e13b8e97e6a3ec5b66f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6f30b759419712ad79aa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
